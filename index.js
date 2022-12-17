39b48389d2e196b063fb0247ffea279d3dffb718
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-16 20:24:00 -0600

    Implemented response for language and software.

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.connection.remoteAddress /* To check validation */
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  
+  res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
